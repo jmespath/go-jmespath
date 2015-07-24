fb665fb7037003885a4874a06be08b2f905530b5
James Saryerwinnie <js@jamesls.com>
2015-07-24 13:17:49 -0700

    Add lex/parse benchmark

--- jmespath/lexer_test.go
+++ jmespath/lexer_test.go
@@ -106,3 +106,56 @@ func TestLexingErrors(t *testing.T) {
 		assert.NotNil(err, fmt.Sprintf("Expected lexing error: %s", tt.msg))
 	}
 }
+
+var exprIdentifier string = "abcdefghijklmnopqrstuvwxyz"
+var exprSubexpr string = "abcdefghijklmnopqrstuvwxyz.abcdefghijklmnopqrstuvwxyz"
+var deeplyNested50 string = "j49.j48.j47.j46.j45.j44.j43.j42.j41.j40.j39.j38.j37.j36.j35.j34.j33.j32.j31.j30.j29.j28.j27.j26.j25.j24.j23.j22.j21.j20.j19.j18.j17.j16.j15.j14.j13.j12.j11.j10.j9.j8.j7.j6.j5.j4.j3.j2.j1.j0"
+var deeplyNested50Pipe string = "j49|j48|j47|j46|j45|j44|j43|j42|j41|j40|j39|j38|j37|j36|j35|j34|j33|j32|j31|j30|j29|j28|j27|j26|j25|j24|j23|j22|j21|j20|j19|j18|j17|j16|j15|j14|j13|j12|j11|j10|j9|j8|j7|j6|j5|j4|j3|j2|j1|j0"
+var deeplyNested50Index string = "[49][48][47][46][45][44][43][42][41][40][39][38][37][36][35][34][33][32][31][30][29][28][27][26][25][24][23][22][21][20][19][18][17][16][15][14][13][12][11][10][9][8][7][6][5][4][3][2][1][0]"
+var deepProjection104 string = "a[*].b[*].c[*].d[*].e[*].f[*].g[*].h[*].i[*].j[*].k[*].l[*].m[*].n[*].o[*].p[*].q[*].r[*].s[*].t[*].u[*].v[*].w[*].x[*].y[*].z[*].a[*].b[*].c[*].d[*].e[*].f[*].g[*].h[*].i[*].j[*].k[*].l[*].m[*].n[*].o[*].p[*].q[*].r[*].s[*].t[*].u[*].v[*].w[*].x[*].y[*].z[*].a[*].b[*].c[*].d[*].e[*].f[*].g[*].h[*].i[*].j[*].k[*].l[*].m[*].n[*].o[*].p[*].q[*].r[*].s[*].t[*].u[*].v[*].w[*].x[*].y[*].z[*].a[*].b[*].c[*].d[*].e[*].f[*].g[*].h[*].i[*].j[*].k[*].l[*].m[*].n[*].o[*].p[*].q[*].r[*].s[*].t[*].u[*].v[*].w[*].x[*].y[*].z[*]"
+var exprQuotedIdentifier string = `"abcdefghijklmnopqrstuvwxyz.abcdefghijklmnopqrstuvwxyz"`
+var quotedIdentifierEscapes string = `"\n\r\b\t\n\r\b\t\n\r\b\t\n\r\b\t\n\r\b\t\n\r\b\t\n\r\b\t"`
+var rawStringLiteral string = `'abcdefghijklmnopqrstuvwxyz.abcdefghijklmnopqrstuvwxyz'`
+
+func BenchmarkLexIdentifier(b *testing.B) {
+	runLexBenchmark(b, exprIdentifier)
+}
+
+func BenchmarkLexSubexpression(b *testing.B) {
+	runLexBenchmark(b, exprSubexpr)
+}
+
+func BenchmarkLexDeeplyNested50(b *testing.B) {
+	runLexBenchmark(b, deeplyNested50)
+}
+
+func BenchmarkLexDeepNested50Pipe(b *testing.B) {
+	runLexBenchmark(b, deeplyNested50Pipe)
+}
+
+func BenchmarLexDeepNested50Index(b *testing.B) {
+	runLexBenchmark(b, deeplyNested50Index)
+}
+
+func BenchmarkLexQuotedIdentifier(b *testing.B) {
+	runLexBenchmark(b, exprQuotedIdentifier)
+}
+
+func BenchmarkLexQuotedIdentifierEscapes(b *testing.B) {
+	runLexBenchmark(b, quotedIdentifierEscapes)
+}
+
+func BenchmarkLexRawStringLiteral(b *testing.B) {
+	runLexBenchmark(b, rawStringLiteral)
+}
+
+func BenchmarkLexDeepProjection104(b *testing.B) {
+	runLexBenchmark(b, deepProjection104)
+}
+
+func runLexBenchmark(b *testing.B, expression string) {
+	lexer := NewLexer()
+	for i := 0; i < b.N; i++ {
+		lexer.Tokenize(expression)
+	}
+}
