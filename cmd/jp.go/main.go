cd8699400873b3bd132421531dbada586d7e9941
James Saryerwinnie <js@jamesls.com>
2015-07-24 14:28:04 -0700

    Refactor jp.go to support stdin and input files

--- cmd/jp.go/main.go
+++ cmd/jp.go/main.go
@@ -1,7 +1,29 @@
+/* Basic command line interface for debug and testing purposes.
+
+Examples:
+
+Only print the AST for the expression:
+
+    jp.go -ast "foo.bar.baz"
+
+Evaluate the JMESPath expression against JSON data from a file:
+
+    jp.go -input /tmp/data.json "foo.bar.baz"
+
+Run JMESPath in test mode, as valid input for the jmespath.test
+test runner.
+
+    jp.go -testmode
+
+*/
 package main
 
-import "fmt"
-import "os"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 import (
 	"encoding/json"
@@ -10,34 +32,69 @@ import (
 	"github.com/kr/pretty"
 )
 
-func main() {
-	parser := jmespath.NewParser()
+func errMsg(msg string, a ...interface{}) int {
+	fmt.Fprintf(os.Stderr, msg, a...)
+	fmt.Fprintln(os.Stderr)
+	return 1
+}
 
-	expression := os.Args[1]
-	input := []byte(os.Args[2])
-	fmt.Println(expression)
-	parsed, err := parser.Parse(expression)
-	if err != nil {
-		fmt.Println("Error parsing expression")
-		fmt.Println(err)
-		return
+func run() int {
+
+	astOnly := flag.Bool("ast", false, "Print the AST for the input expression and exit.")
+	inputFile := flag.String("input", "", "Filename containing JSON data to search. If not provided, data is read from stdin.")
+	testMode := flag.Bool("testmode", false, "Run JMESPath in test mode for the JMESPath compliance test runner.")
+
+	flag.Parse()
+	args := flag.Args()
+
+	if *astOnly && *testMode {
+		return errMsg("-ast and -testmode are mutually exclusive.")
 	}
-	pretty.Print(parsed)
-	fmt.Println("")
+	if *astOnly {
+		if len(args) != 1 {
+			return errMsg("Expected a single argument (the JMESPath expression).")
+		}
+		expression := args[0]
+		parser := jmespath.NewParser()
+		parsed, err := parser.Parse(expression)
+		if err != nil {
+			return errMsg("Error parsing expression (%s): %s", expression, err)
+		}
+		pretty.Print(parsed)
+		fmt.Println("")
+		return 0
+	}
+
+	expression := args[0]
+	var inputData []byte
+	var err error
+	if *inputFile != "" {
+		inputData, err = ioutil.ReadFile(*inputFile)
+		if err != nil {
+			return errMsg("Error loading file %s: %s", *inputFile, err)
+		}
+	} else {
+		// If an input data file is not provided then we read the
+		// data from stdin.
+		inputData, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			return errMsg("Error reading from stdin: %s", err)
+		}
+	}
 	var data interface{}
-	json.Unmarshal(input, &data)
+	json.Unmarshal(inputData, &data)
 	result, err := jmespath.Search(expression, data)
 	if err != nil {
-		fmt.Println("Error executing expression")
-		fmt.Println(err)
-		return
+		return errMsg("Error executing expression: %s", err)
 	}
-	fmt.Println(result)
-	toJSON, err := json.Marshal(result)
+	toJSON, err := json.MarshalIndent(result, "", "  ")
 	if err != nil {
-		fmt.Println("Error serializing JSON")
-		fmt.Println(err)
-		return
+		return errMsg("Error serializing result to JSON: %s", err)
 	}
 	fmt.Println(string(toJSON))
+	return 0
+}
+
+func main() {
+	os.Exit(run())
 }
