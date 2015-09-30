8718ef0b8fc22d275cec4b3edd723b7aacfe19a4
James Saryerwinnie <js@jamesls.com>
2015-09-29 22:07:12 -0700

    Fix parsing crash for expref parsing
    Thanks, go-fuzz.

--- parser.go
+++ parser.go
@@ -388,7 +388,7 @@ func (p *Parser) nud(token token) (ASTNode, error) {
 	case tExpref:
 		expression, err := p.parseExpression(bindingPowers[tExpref])
 		if err != nil {
-			return ASTNode{}, nil
+			return ASTNode{}, err
 		}
 		return ASTNode{nodeType: ASTExpRef, children: []ASTNode{expression}}, nil
 	case tNot:
